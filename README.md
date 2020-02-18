4831d3245040b684fa2eb279315b3b9f44ad397c
Sheldon Ip <4224778+sheldonip@users.noreply.github.com>
2020-02-18 22:35:08 +0800

    Update data and graph type

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ You know how
 # References
 * [Radial Dendrogram / D3 / Observable](https://observablehq.com/@d3/radial-dendrogram)
 * [Org Chart (Radial -> Dendrogram Transition) - bl.ocks.org](https://bl.ocks.org/cjhin/7023f2df4f9165b6e4ed8e07ab7b968a)
+* [Tidy Tree vs Dendogram - bl.ocks.org](https://bl.ocks.org/Andrew-Reid/c7ae41a98b8cbb38f1febf13deb9d294)
--- app.js
+++ app.js
@@ -1,21 +1,31 @@
 (function () {
     const genderColours = ["red", "blue"];
-    let svg = d3.select("svg.radical"),
-        g = svg.append("g").attr("transform", "translate(" + 80 + "," + 0 + ")");
-    const width = +svg.attr("width"), height = +svg.attr("height");
-
-    let stratify = d3.stratify()
-        .id(function (d) { return d.casenumber; })
-        .parentId(function (d) { return d.parent; });
-
-    function project(x, y) {
-        let angle = (x - 90) / 180 * Math.PI, radius = y;
-        return [radius * Math.cos(angle) + width / 2.0, radius * Math.sin(angle) + height / 2.0];
-    }
-
     d3.csv("data.csv").then(function (data) {
-        let color = d3.scaleOrdinal(genderColours);
+        const width = 1200;
+        const height = 1000;
+        const transitionTime = 2000;
 
+        const margin = { left: 100, top: 100, right: 50, bottom: 50 }
+
+        let color = d3.scaleOrdinal(genderColours);
+        let radialMode = true;
+
+        let svg = d3.select("svg.radical")
+            .attr("width", width)
+            .attr("height", height);
+
+        let g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        let stratify = d3.stratify()
+            .id(function (d) { return d.casenumber; })
+            .parentId(function (d) { return d.parent; });
+
+        let root = stratify(data)
+            .sort(function (a, b) {
+                return (a.data.gender.localeCompare(b.data.gender) || b.height - a.height);
+            });
+
+        // Setup legend
         // Title
         svg.append("text")
             .attr("x", "20")
@@ -23,7 +33,6 @@
             .attr("class", "chart-title")
             .text("性別");
 
-        // Setup legend
         const legendDotSize = 30;
         let legendWrapper = svg.append("g")
             .attr("class", "legend")
@@ -58,112 +67,185 @@
             .merge(legendDot)
             .style("fill", function (d) { return color(d); });
 
+        // Tree
+        let tree = d3.tree()
+            .size([width - margin.left - margin.right, height - margin.top - margin.bottom]);
+
+        // Cluster	
+        let cluster = d3.cluster()
+            .size([height - margin.top - margin.bottom, width - margin.left - margin.right]);
+
+        // Set initial radial tree
+        tree.size([2 * Math.PI, height / 2.1]);
+        let link = g.selectAll(".link")
+            .data(tree(root).links())
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("id", function (d) { return "link-" + d.target.id; })
+            .attr("fill", "none")
+            .attr("stroke", "#ccc")
+            .attr("d", d3.linkRadial()
+                .angle(function (d) { return d.x; })
+                .radius(function (d) { return d.y; }));
+
+        let node = g.selectAll(".node")
+            .data(root.descendants())
+            .enter().append("g")
+            .attr("class", function (d) { return "node" + (d.children ? " node-internal" : " node-leaf"); })
+            .attr("id", function (d) { return "node-" + d.data.casenumber; })
+            .attr("transform", function (d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
+            .on("mouseover", function (d) { mouseovered(d); })
+            .on("mouseout", function (d) { mouseout(); });
+
+        node.append("circle")
+            .attr("r", 6)
+            .attr("fill", function (d) {
+                if (d.data.gender == "") {
+                    return "grey";
+                }
+                return color(d.data.gender);
+            });;
+
+        node.append("text")
+            .text(function (d) {
+                switch (parseInt(d.id)) {
+                    case -1:
+                        return "輸入";
+                    case 0:
+                        return "本地";
+                    default:
+                        return d.data.age + "歳" + d.data.gender + " (#" + d.data.casenumber + ")";
+                }
+            })
+            .attr('y', -10)
+            .attr('x', -10)
+            .attr('text-anchor', 'middle');
+
         // Interaction with button
-        let radialMode = true;
         document.getElementById("toggle-button").onclick = function () { toggleMode() };
 
-        let toggleMode = function () {
-            let tree;
+        function verticalTree() {
+            g.transition().attr("transform", 'translate(' + margin.left + ',' + margin.right + ')').duration(transitionTime);
+            tree.size([width - margin.left - margin.right, height - margin.top - margin.bottom]);
+            link.data(tree(root).links())
+                .transition()
+                .attr("d", d3.linkVertical()
+                    .x(function (d) { return d.x; })
+                    .y(function (d) { return d.y; }))
+                .duration(transitionTime);
 
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+                .duration(transitionTime)
+        }
+
+        function horizontalTree() {
+            g.transition().attr("transform", 'translate(' + margin.left + ',' + margin.right + ')').duration(transitionTime);
+            tree.size([height - margin.top - margin.bottom, width - margin.left - margin.right]);
+            link.data(tree(root).links())
+                .transition()
+                .attr("d", d3.linkHorizontal()
+                    .x(function (d) { return d.y; })
+                    .y(function (d) { return d.x; }))
+                .duration(transitionTime);
+
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
+                .duration(transitionTime);
+        }
+
+        function radialTree() {
+            g.transition().attr("transform", "translate(" + width / 2 + "," + height / 2 + ")").duration(transitionTime);
+            tree.size([2 * Math.PI, height / 2.1]);
+            link.data(tree(root).links())
+                .transition()
+                .attr("d", d3.linkRadial()
+                    .angle(function (d) { return d.x; })
+                    .radius(function (d) { return d.y; }))
+                .duration(transitionTime);
+
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
+                .duration(transitionTime);
+        }
+
+        function horizontalCluster() {
+            g.transition().attr("transform", 'translate(' + margin.left + ',' + margin.right + ')').duration(transitionTime);
+            cluster.size([height - margin.top - margin.bottom, width - margin.left - margin.right]);
+            link
+                .data(cluster(root).links())
+                .transition()
+                .attr("d", function (d) {
+                    console.log(d);
+                    return "M" + d.source.y + "," + d.source.x
+                        + "C" + (d.source.y + 100) + "," + d.source.x
+                        + " " + (d.source.y + 100) + "," + d.target.x
+                        + " " + d.target.y + "," + d.target.x;
+                })
+                .duration(transitionTime)
+
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; })
+                .duration(transitionTime);
+        }
+
+        function verticalCluster() {
+            g.transition().attr("transform", 'translate(' + margin.left + ',' + margin.right + ')').duration(transitionTime);
+            cluster.size([width - margin.left - margin.right, height - margin.top - margin.bottom]);
+            link
+                .data(cluster(root).links())
+                .transition()
+                .attr("d", function (d) {
+                    console.log(d);
+                    return "M" + d.source.x + "," + d.source.y
+                        + "C" + d.source.x + "," + (d.source.y + 60)
+                        + " " + d.target.x + "," + (d.source.y + 60)
+                        + " " + d.target.x + "," + d.target.y;
+                })
+                .duration(transitionTime)
+
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; })
+                .duration(transitionTime);
+        }
+
+        function radialCluster() {
+            g.transition().attr("transform", "translate(" + width / 2 + "," + height / 2 + ")").duration(transitionTime);
+            cluster.size([2 * Math.PI, height / 2 - 40]);
+
+            link
+                .data(cluster(root).links())
+                .transition()
+                .attr("d", function (d) {
+                    return "M" + radialPoint(d.source.x, d.source.y)
+                        + "C" + radialPoint(d.source.x, (d.target.y + d.source.y) / 2)
+                        + " " + radialPoint(d.target.x, (d.target.y + d.source.y) / 2)
+                        + " " + radialPoint(d.target.x, d.target.y);
+                })
+                .duration(transitionTime)
+
+            node.transition()
+                .attr("transform", function (d) { return "translate(" + radialPoint(d.x, d.y) + ")"; })
+                .duration(transitionTime);
+        }
+
+        function radialPoint(x, y) {
+            return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
+        }
+
+        function toggleMode() {
             if (radialMode) {
-                tree = d3.cluster()
-                    .size([360, 320]) // degrees, radius
-                    .separation(function (a, b) { return (a.parent == b.parent ? 1 : 2) / a.depth; });
+                horizontalTree();
+                radialMode = false;
             } else {
-                tree = d3.cluster()
-                    .size([820, 700]); // height, width
+                radialTree();
+                radialMode = true;
             }
-
-            let root = stratify(data)
-                .sort(function (a, b) {
-                    return (a.data.gender.localeCompare(b.data.gender) || b.height - a.height);
-                });
-
-            tree(root);
-
-            let link = g.selectAll(".link")
-                .data(root.descendants().slice(1));
-
-            let node = g.selectAll(".node").data(root.descendants());
-
-            node.group = node.enter().append("g")
-                .attr("class", function (d) { return "node" + (d.children ? " node-internal" : " node-leaf"); })
-                .attr("id", function (d) { return "node-" + d.data.casenumber; })
-                .on("mouseover", function (d) { mouseovered(d); })
-                .on("mouseout", function (d) { mouseout(); });
-
-            if (radialMode) {
-                link.enter().append("path")
-                    .attr("class", "link")
-                    .attr("id", function (d) { return "link-" + d.data.casenumber; })
-                    .merge(link).transition().duration(1000)
-                    .attr("d", function (d) {
-                        return "M" + project(d.x, d.y)
-                            + "C" + project(d.x, (d.y + d.parent.y) / 2)
-                            + " " + project(d.parent.x, (d.y + d.parent.y) / 2)
-                            + " " + project(d.parent.x, d.parent.y);
-                    });
-
-                node.group.merge(node).transition().duration(1000)
-                    .attr("transform", function (d) { return "translate(" + project(d.x, d.y) + ")"; });
-
-                node.group.append("text")
-                    .text(function (d) {
-                        switch (parseInt(d.id)) {
-                            case -1:
-                                return "輸入";
-                            case 0:
-                                return "本地";
-                            default:
-                                return d.data.age + "歳" + d.data.gender + " (#" + d.data.casenumber + ")";
-                        }
-                    })
-                    .merge(node.select("text")).transition().duration(1000)
-                    .attr("dy", function(d) { return d.children ? -5 : 0; })
-                    .attr("x", function (d) { return d.x < 180 === !d.children ? 12 : -1 * (this.getBBox().width + 12); })
-                    .attr("transform", function (d) { return "rotate(" + (d.x < 180 ? d.x - 90 : d.x + 90) + ")"; });
-            } else {
-                link.enter().append("path")
-                    .attr("class", "link")
-                    .attr("id", function (d) { return "link-" + d.data.casenumber; })
-                    .merge(link).transition().duration(1000)
-                    .attr("d", function (d) {
-                        return "M" + d.y + "," + d.x
-                            + "C" + (d.parent.y + 100) + "," + d.x
-                            + " " + (d.parent.y + 100) + "," + d.parent.x
-                            + " " + d.parent.y + "," + d.parent.x;
-                    });
-
-                node.group.merge(node).transition().duration(1000)
-                    .attr("transform", function (d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-                node.group.append("text")
-                    .text(function (d) { return d.data.name; })
-                    .merge(node.select("text")).transition().duration(1000)
-                    .attr("dy", function(d) { return d.children ? -5 : 3;})
-                    .attr("x", 12)
-                    .attr("transform", function (d) { return "rotate(0)"; });
-            }
-
-            node.group.append("circle")
-                .attr("r", 6)
-                .attr("fill", function (d) {
-                    if (d.data.gender == "") {
-                        return "grey";
-                    }
-                    return color(d.data.gender);
-                });
-
-            // Toggle state
-            radialMode = !radialMode;
-        };
-
-        // run radial mode first!
-        toggleMode();
+        }
     }).catch(function (error) {
         // handle error
         console.log(error);
-    })
+    });
 
     function mouseovered(d) {
         let relatedIdSet = getAllParentIds(d);
@@ -186,11 +268,11 @@
 
     function mouseout() {
         let els = document.getElementsByClassName("node-active");
-        while(els.length > 0) {
+        while (els.length > 0) {
             els[0].classList.remove("node-active");
         }
         els = document.getElementsByClassName("link-active");
-        while(els.length > 0) {
+        while (els.length > 0) {
             els[0].classList.remove("link-active");
         }
     }
